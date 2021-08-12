3d387c2534794a32b308872bcc9fe1275d782996
Ahmedbinfaisal <67901435+AhmedFaisal11@users.noreply.github.com>
2021-08-12 04:20:15 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is Your Name : ");
+            var name = Console.Read();
+            var currentDate = DateTime.Now;
+
+            Console.WriteLine($"Hello , {name}  on {currentDate:d} at {currentDate:t} ");
+            Console.WriteLine($"Press any Key to Exit....");
+            Console.ReadKey(true);
+        }
+    }
+}
